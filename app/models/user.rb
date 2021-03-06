e3beb853cfd3cbc40e9543a09e15d56153f0efac
keisuke nakamura <nk0207.pg@gmail.com>
2021-03-06 23:45:34 +0000

    create users/index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
+  
   validates :name,  presence: true, length: { maximum:50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,7 +11,7 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -32,7 +33,6 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
    end
 
-  
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
